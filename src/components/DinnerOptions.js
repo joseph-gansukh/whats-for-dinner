b571f74afcdbb7db61a750a5ea1c478b163ab79f
joseph-gansukh <g.sukhbat@gmail.com>
2020-01-29 23:29:57 -0600

    using localstorage added data persistence

--- src/components/DinnerOptions.js
+++ src/components/DinnerOptions.js
@@ -4,6 +4,7 @@ import DinnerOption from './DinnerOption'
 export default function DinnerOptions(props) {
   return (
     <div>
+      <button onClick={props.handleRemoveAll}>Remove All</button>
       {props.options.map(option => (
         <DinnerOption key={option} dinner={option} />
       ))}
