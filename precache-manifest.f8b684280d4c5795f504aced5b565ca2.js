4df4a5c537120baea2eb980c2dc3b798142e78a0
joseph-gansukh <g.sukhbat@gmail.com>
2020-01-31 20:44:56 -0600

    Updates

---
+++ precache-manifest.f8b684280d4c5795f504aced5b565ca2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989e929953f7ead04eab6e6d857e5255",
+    "url": "/whats-for-dinner/index.html"
+  },
+  {
+    "revision": "c51ef039b2f3287c3365",
+    "url": "/whats-for-dinner/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b9b3468f34f9e7463bca",
+    "url": "/whats-for-dinner/static/css/main.a7f943d4.chunk.css"
+  },
+  {
+    "revision": "c51ef039b2f3287c3365",
+    "url": "/whats-for-dinner/static/js/2.7bd65750.chunk.js"
+  },
+  {
+    "revision": "28ac78fef8675bfe412d94015e05d458",
+    "url": "/whats-for-dinner/static/js/2.7bd65750.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9b3468f34f9e7463bca",
+    "url": "/whats-for-dinner/static/js/main.45b27c6e.chunk.js"
+  },
+  {
+    "revision": "47d3707592b114948eef",
+    "url": "/whats-for-dinner/static/js/runtime-main.026e1b2d.js"
+  }
+]);
