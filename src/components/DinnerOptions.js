672c7f6fb499302753f9520f27280a5e7e81babe
Joseph S Gansukh <52771685+joseph-gansukh@users.noreply.github.com>
2020-06-12 23:03:48 -0500

    Update DinnerOptions.js

--- src/components/DinnerOptions.js
+++ src/components/DinnerOptions.js
@@ -5,7 +5,7 @@ export default function DinnerOptions(props) {
   return (
     <div>
       <div className="widget-header">
-        <h3 className="widget-header__title">Your Options</h3>
+        <h3 className="widget-header__title">Your Options:</h3>
         <button className="button button--link" onClick={props.handleRemoveAll}>Remove All</button>
       </div>
       {props.options.length === 0 && <p className="widget__message">Please add some options to get started</p>}
